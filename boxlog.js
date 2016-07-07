224af494e508452f7bbbf186859f848492b74523
Solo Traze <traze.solo.dev@gmail.com>
2016-07-07 23:06:27 +0530

    Added basic logic files

---
+++ boxlog.js
@@ -0,0 +1,15 @@
+/*
+ * Log inside shapes like circle or rectangles. Destination might be files.
+ */
+
+
+var logBox = function (msg) {
+  console.log(msg);
+};
+
+var logCircle = function (msg) {
+  console.log(msg);
+};
+
+exports.logBox = logBox;
+exports.logCircle = logCircle;
